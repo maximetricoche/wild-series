122985e2a9c95a4c47706d698ed39bc17bc6fddd
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-12-09 12:02:50 +0100

    feat: create route sayWelcome

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,6 @@
 // Load environment variables from .env file
 import "dotenv/config";
+import type { RequestHandler } from "express";
 
 // Check database connection
 // Note: This is optional and can be removed if the database connection
@@ -20,3 +21,9 @@ app
   .on("error", (err: Error) => {
     console.error("Error:", err.message);
   });
+
+const sayWelcome: RequestHandler = (req, res) => {
+  res.send("Welcome to Wild Series !");
+};
+
+app.get("/", sayWelcome);
