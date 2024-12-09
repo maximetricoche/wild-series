2e5469161d757afaccbe8871f2143e9976d84c8f
Maxime TRICOCHE <maxime.trche@gmail.com>
2024-12-09 16:39:42 +0100

    feat: create categories modules for routes api

---
+++ server/src/modules/category/categoryActions.ts
@@ -0,0 +1,29 @@
+const categories = [
+  {
+    id: 1,
+    name: "Comédie",
+  },
+  {
+    id: 2,
+    name: "Science-Fiction",
+  },
+];
+
+import type { RequestHandler } from "express";
+
+const browse: RequestHandler = (req, res) => {
+  res.json(categories);
+};
+
+const read: RequestHandler = (req, res) => {
+  const parsedId = Number.parseInt(req.params.id);
+
+  const category = categories.find((c) => c.id === parsedId);
+  if (category != null) {
+    res.json(category);
+  } else {
+    res.sendStatus(404);
+  }
+};
+
+export default { browse, read };
